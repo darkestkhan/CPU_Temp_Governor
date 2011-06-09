4b3b8f88940db3fee8673354d4c97eabfa5d12ec
darkestkhan <darkestkhan@gmail.com>
2011-06-09 19:13:47 +0000

    added additional compilation unit to make.sh
    this function is in case if someone doesn't have working gprbuild
    (just as I now... )

--- make.sh
+++ make.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env dash
 
+fallback_compilation () {
+  gnatmake -O3 -m64 -mssse3 -march=core2 -fomit-frame-pointer -Wall -gnat05 -gnata -gnatE -gnato -fstack-check src/start_gov.adb
+  mv *.ali *.o obj/ 
+}
+
 if ! [ -d ./obj ]
 then
   mkdir obj
 fi
-gprbuild -Pgov.gpr
+
+#gprbuild -Pgov.gpr
+
+fallback_compilation
